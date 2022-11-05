ba1e9c4a36094be2e335ed636ed8a16d0f8c9372
AmandaCasadem <amanda12kcb@gmail.com>
2022-11-05 15:37:37 -0300

    Footer

---
+++ src/footer/Footer.jsx
@@ -0,0 +1,48 @@
+
+import React from "react"
+import "./Footer.css"
+
+const Footer =() => {
+   return (
+<>
+<footer>
+<div class="main-content">
+			<div class="left box">
+				<h2> Sobre nós</h2>
+				<div class="content">
+					<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet iste facilis harum eos vel incidunt distinctio corrupti iure? Rem</p>
+				</div>
+			</div>
+			<div class="center box">
+				<h2>Endereço</h2>
+				<div class="content">
+					<div class="place">
+						
+						<span class="text">Av.Brasil, Teresópolis ,RJ</span>
+					</div>
+
+					<div class="phone">
+						
+						<span class="text">+55 21 9999-9999</span>
+					</div>
+
+					<div class="email">
+						
+						<span class="text">walmartdashoope@exemplo.com</span>
+					</div>
+				</div>
+			</div>
+			
+		</div>
+		<div class="bottom">
+			<div class="center">
+				<span class="credit">Criado por <a href="#">WalMarte da Shoope</a></span>
+				<span class="far fa-copyright"> 2022 Todos os diteitos reservados</span>
+			</div>
+		</div>
+	</footer>
+
+</>
+   )
+}
+export default  Footer 
